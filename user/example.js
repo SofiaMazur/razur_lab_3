28ebdae50b0dc95195105e138f1acbc27cc4c4f6
SofiaMazur <43069027+SofiaMazur@users.noreply.github.com>
2020-12-21 01:05:15 +0200

    Update example.js

--- user/example.js
+++ user/example.js
@@ -1,31 +1,18 @@
 const forumsClient = require('./forumsClient');
 
 const testData = [
-  ['addForum', 'Adding a new forum (no args)', []],
-  ['addForum', 'Adding a new forum (no name)', [undefined, 'golang']],
-  ['addForum', 'Adding a new forum (no topic)', ['Gophers', undefined]],
-  ['addForum', 'Adding a new forum (empty name)', ['', 'Books']],
-  ['addForum', 'Adding a new forum (empty topic)', ['Gophers', '']],
-  ['addForum', 'Adding a new forum (name exists)', ['Book enjoyer', 'reading']],
-  ['addForum', 'Adding a new forum (topic exists)', ['Film Lover', 'Movies']],
-  ['addForum', 'Adding a new forum', ['Gophers', 'golang']],
+  ['addSite', 'Adding a new site (no args)', []],
+  ['addSite', 'Adding a new site (no name)', [undefined, 'golang']],
+  ['addSite', 'Adding a new site (no topic)', ['Gophers', undefined]],
+  ['addSite', 'Adding a new site (empty name)', ['', 'Books']],
+  ['addSite', 'Adding a new site (empty topic)', ['Gophers', '']],
+  ['addSite', 'Adding a new site (name exists)', ['Book enjoyer', 'reading']],
+  ['addSite', 'Adding a new site (topic exists)', ['Film Lover', 'Movies']],
+  ['addSite', 'Adding a new site', ['Gophers', 'golang']],
   ['addUser', 'Adding a new user (no args)', []],
   ['addUser', 'Adding a new user (no name)', [undefined, ['golang']]],
   ['addUser', 'Adding a new user (no interests)', ['Barbara', undefined]],
   ['addUser', 'Adding a new user (empty string interests)', ['Barbara', ['Movies', '']]],
-  ['addUser', 'Adding a new user (empty name)', ['', ['golang']]],
-  ['addUser', 'Adding a new user (existing)', ['Bob', ['movies', 'reading']]],
-  ['addUser', 'Adding a new user', ['Barbara', ['golang']]],
-  ['getUser', 'Getting user (not registered)', ['Bill']],
-  ['getUser', 'Getting user (empty name)', ['']],
-  ['getUser', 'Getting user (null name)', [null]],
-  ['getUser', 'Getting user', ['Barbara']],
-  ['getForum', 'Getting forum (not registered)', ['Java Hell']],
-  ['getForum', 'Getting forum (empty name)', ['']],
-  ['getForum', 'Getting forum (null name)', [null]],
-  ['getForum', 'Getting forum', ['Gophers']],
-  ['getUsers', 'Getting all users', []],
-  ['getForums', 'Getting all forums', []]
 ];
 
 const sendTestResponses = async () => {
