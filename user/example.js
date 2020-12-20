484fe33cac38874115424d041a7932bca73a836f
SofiaMazur <43069027+SofiaMazur@users.noreply.github.com>
2020-12-21 01:09:40 +0200

    Update example.js

--- user/example.js
+++ user/example.js
@@ -1,18 +1,18 @@
 const forumsClient = require('./forumsClient');
 
 const testData = [
-  ['addSite', 'Adding a new site (no args)', []],
-  ['addSite', 'Adding a new site (no name)', [undefined, 'golang']],
-  ['addSite', 'Adding a new site (no topic)', ['Gophers', undefined]],
-  ['addSite', 'Adding a new site (empty name)', ['', 'Books']],
-  ['addSite', 'Adding a new site (empty topic)', ['Gophers', '']],
-  ['addSite', 'Adding a new site (name exists)', ['Book enjoyer', 'reading']],
-  ['addSite', 'Adding a new site (topic exists)', ['Film Lover', 'Movies']],
+  ['addSite', 'Adding a new site (no topic)', []],
+  ['addSite', 'Adding a new site (empty topic)', [undefined, 'js']],
+  ['addSite', 'Adding a new site (no topic)', ['no', undefined]],
+  ['addSite', 'Adding a new site (empty name)', ['', 'magazines']],
+  ['addSite', 'Adding a new site (no name)', ['no', '']],
+  ['addSite', 'Adding a new site (name exists)', ['Magazines', 'reading']],
+  ['addSite', 'Adding a new site (topic exists)', ['TV-show', 'Films']],
   ['addSite', 'Adding a new site', ['Gophers', 'golang']],
   ['addUser', 'Adding a new user (no args)', []],
   ['addUser', 'Adding a new user (no name)', [undefined, ['golang']]],
-  ['addUser', 'Adding a new user (no interests)', ['Barbara', undefined]],
-  ['addUser', 'Adding a new user (empty string interests)', ['Barbara', ['Movies', '']]],
+  ['addUser', 'Adding a new user (no topic)', ['Dr. Strange', undefined]],
+  ['addUser', 'Adding a new user (empty string interests)', ['Dr. Strange', ['Films', '']]],
 ];
 
 const sendTestResponses = async () => {
