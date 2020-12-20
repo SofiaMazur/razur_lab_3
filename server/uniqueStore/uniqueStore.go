76ee7e9ef07ef8de9db9fb5106278403591fa847
SofiaMazur <43069027+SofiaMazur@users.noreply.github.com>
2020-12-21 01:28:36 +0200

    Update uniqueStore.go

--- server/uniqueStore/uniqueStore.go
+++ server/uniqueStore/uniqueStore.go
@@ -1,42 +1,42 @@
 пакет generalStore
 
 импорт (
-	"база данных / sql"
-	"github.com/GVG/l3/server/tools"
+	"database/sql"
+	"github.com/SofiaMazur/razur_lab_3/server/tools"
 	_ "github.com/lib/pq"
 )
 
-type  UniqueStore  struct {
-	FStore  * ForumStore
-	UStore  * UserStore
+type UniqueStore struct {
+	FStore *ForumStore
+	UStore *UserStore
 }
 
-func  NewUniqueStore ( db  * sql. DB ) * GeneralStore {
-	fstore  : =  NewForumStore ( db )
-	ustore  : =  NewUserStore ( db )
-	return  & UniqueStore { FStore : fstore , UStore : ustore }
+func NewGeneralStore(db *sql.DB) *GeneralStore {
+	fstore := NewForumStore(db)
+	ustore := NewUserStore(db)
+	return &UniqueStore{FStore: fstore, UStore: ustore}
 }
 
-func ( gs  * UniqueStore ) ListForums () ( * tools. Forums , error ) {
-	вернуть  gs . FStore . ListForums ()
+func (gs *UniqueStore) ListForums() (*tools.Forums, error) {
+	return gs.FStore.ListForums()
 }
 
-func ( gs  * UniqueStore ) FindForumByName ( строка имени  ) ( * tools. Forums , error ) {
-	вернуть  gs . FStore . FindForumByName ( имя )
+func (gs *UniqueStore) FindForumByName(name string) (*tools.Forums, error) {
+	return gs.FStore.FindForumByName(name)
 }
 
-func ( gs  * UniqueStore ) FindUserByName ( строка имени  ) ( * tools. Users , error ) {
-	вернуть  gs . UStore . FindUserByName ( имя )
+func (gs *UniqueStore) FindUserByName(name string) (*tools.Users, error) {
+	return gs.UStore.FindUserByName(name)
 }
 
-func ( gs  * UniqueStore ) CreateForum ( name , topicKeyword  string ) error {
-	вернуть  gs . FStore . CreateForum ( название , тема , ключевое слово )
+func (gs *UniqueStore) CreateForum(name, topicKeyword string) error {
+	return gs.FStore.CreateForum(name, topicKeyword)
 }
 
-func ( gs  * UniqueStore ) ListUsers () ( * tools. Users , error ) {
-	вернуть  gs . UStore . ListUsers ()
+func (gs *UniqueStore) ListUsers() (*tools.Users, error) {
+	return gs.UStore.ListUsers()
 }
 
-функ ( гс  * UniqueStore ) CreateUser ( имя пользователя  струнные , интересы [] строка ) ошибки {
-	вернуть  gs . UStore . CreateUser ( имя пользователя , интересы )
+func (gs *UniqueStore) CreateUser(username string, interests []string) error {
+	return gs.UStore.CreateUser(username, interests)
 }
